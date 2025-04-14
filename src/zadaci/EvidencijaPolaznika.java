d141f4c8246348f6edcd101b2df7fc83eb8c872d
Iv00vI <goran.ivo11@gmail.com>
2025-04-14 21:05:33 +0200

    Initial commit

---
+++ src/zadaci/EvidencijaPolaznika.java
@@ -0,0 +1,10 @@
+package zadaci;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+       
+    }
+}
---
+++ src/zadaci/Polaznik.java
@@ -0,0 +1,11 @@
+public class Polaznik {
+    String ime;
+
+    public Polaznik(String name) {
+        this.ime = name;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+}
